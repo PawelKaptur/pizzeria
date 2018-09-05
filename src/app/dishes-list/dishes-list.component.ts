b46e272211603917124dc99b5b4ea1dd2d37c162
Pawel Kaptur <pawel.kaptur@protonmail.com>
2018-09-05 13:28:14 +0200

    added models, and dishes list component

---
+++ src/app/dishes-list/dishes-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Dish} from "../models/dish.model";
+
+@Component({
+  selector: 'app-dishes-list',
+  templateUrl: './dishes-list.component.html',
+  styleUrls: ['./dishes-list.component.scss']
+})
+export class DishesListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
